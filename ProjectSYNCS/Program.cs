c0883ad00a1507ca6d1d4475b1f6d34a50ebe06b
Romain MELISEN <romain.melisen@chuliege.be>
2026-06-16 10:21:27 +0200

    Added appsettings.json + added configuration setup and dependency injection setup (WIP) in Program.cs

--- ProjectSYNCS/Program.cs
+++ ProjectSYNCS/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using LazyRain.Data;
+using LazyRain.Services;
+using Microsoft.EntityFrameworkCore;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((ctx, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false);
+        // Optional, overrides overlapping keys in appsettings.json
+        config.AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true);
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        var config = ctx.Configuration;
+
+        var socketConfig = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+            LogLevel = LogSeverity.Info,
+            MessageCacheSize = 100
+        };
+        services.AddSingleton(socketConfig);
+        services.AddSingleton<DiscordSocketClient>();
+
+        var interactionConfig = new InteractionServiceConfig
+        {
+            UseCompiledLambda = true,
+            LogLevel = LogSeverity.Info
+        };
+        services.AddSingleton(interactionConfig);
+        services.AddSingleton<InteractionService>();
+
+        //var dbPath = config["Database:Path"] ?? "ProjectSYNCS.db";
+        //services.AddDbContext<AppDbContext>(options =>
+        //    options.UseSqlite($"Data Source={dbPath}"),
+        //    ServiceLifetime.Transient);
+
+        //services.AddTransient<EventService>();
+
+        //services.AddHostedService<BotService>();
+        //services.AddHostedService<ReminderService>();
+    })
+    .Build();
