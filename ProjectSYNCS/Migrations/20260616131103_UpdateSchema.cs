83355468192d884e3cec63292b7e2b2920eb9cbe
Romain MELISEN <romain.melisen@chuliege.be>
2026-06-16 15:11:28 +0200

    Updated Migration

---
+++ ProjectSYNCS/Migrations/20260616131103_UpdateSchema.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectSYNCS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GameName",
+                table: "SessionEvents",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "SessionEvents",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "SessionEvents");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "SessionEvents",
+                newName: "GameName");
+        }
+    }
+}
